79ac8a4f3cc7d2a89dcde254ba7dc786eebb8121
hackwithrama <rajram92@hotmail.com>
2024-06-29 09:12:41 +0530

    Initial Commit

---
+++ FruitsApp/FruitsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsAppApp.swift
+//  FruitsApp
+//
+//  Created by Ramachandran Varadaraju on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FruitsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
